2658371e4951f0298d55bdc7a0e549a8e3a5ea72
LuciLua <luci.lua81@gmail.com>
2021-03-26 15:32:38 -0300

    Portfolio

---
+++ app.js
@@ -0,0 +1,28 @@
+var text = document.getElementById('text');
+var word = text.getElementsByTagName('span');
+var i = 0;
+
+function rotator (){
+    word[i].style.display = 'none';
+    i = (i + 1) % word.length;
+    word[i].style.display = 'initial';
+}
+
+setInterval(rotator, 1500);
+
+var img = document.getElementById('left')
+var im = img.getElementsByTagName('img')
+var o = 0;
+
+function rotatorr (){
+    im[o].style.display = 'none';
+    o = (o + 1) % im.length;
+    im[o].style.display = 'initial';
+}
+
+setInterval(rotatorr, 3000);
+
+window.addEventListener('scroll', function(){
+    const header = document.getElementById('menu')
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
