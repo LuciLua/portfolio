32bf89f3260f2eb0316eb914fd1ff115755b4beb
LuciLua <luci.lua81@gmail.com>
2021-03-28 22:10:32 -0300

    :star2: Portfólio | Cards versão 2 :star2:

--- app.js
+++ app.js
@@ -28,3 +28,37 @@ function abreMenu(){
     menuA.classList.toggle("viraisso")
 }
 
+var fundoPerso = document.getElementById('imagem')
+var te = document.getElementById('p-i')
+
+function toggleMenuA(){
+    let menu = document.querySelector('#quad-1');
+    let toggle = document.querySelector('#pega');
+    menu.classList.toggle('active');
+    toggle.classList.toggle('active');
+    // fundoPerso.style.backgroundImage="img/snake.png"
+    fundoPerso.classList.toggle('neutrum')
+    te.classList.toggle('p-i-none')
+
+}
+
+function toggleMenuB(){
+    let menu2 = document.querySelector('#quad-2');
+    let toggle2 = document.querySelector('#pega2');
+    menu2.classList.toggle('active');
+    toggle2.classList.toggle('active');
+    fundoPerso.classList.toggle('dialogos')
+    te.classList.toggle('p-i-none')
+}
+
+
+function toggleMenuC(){
+    let menu3 = document.querySelector('#quad-3');
+    let toggle3 = document.querySelector('#pega3');
+    menu3.classList.toggle('active');
+    toggle3.classList.toggle('active');
+    fundoPerso.classList.toggle('pomba')
+    te.classList.toggle('p-i-none')
+}
+
+
