e3357640df0b831398dacef807c02123f06d062c
LuciLua <luci.lua81@gmail.com>
2021-04-13 17:08:35 -0300

    :star2: Portfólio | Refazendo :star2:

--- app.js
+++ app.js
@@ -10,40 +10,12 @@
 
 // setInterval(rotator, 1500);
 
+window.addEventListener('scroll', function(){
+    const header = document.getElementById('memul')
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
 function abreMenu(){
     var menuA = document.getElementById('memul')
     menuA.classList.toggle("viraisso")
-}
-
-var fundoPerso = document.getElementById('imagem')
-var te = document.getElementById('p-i')
-
-function toggleMenuA(){
-    let menu = document.querySelector('#quad-1');
-    let toggle = document.querySelector('#pega');
-    menu.classList.toggle('active');
-    toggle.classList.toggle('active');
-    fundoPerso.classList.toggle('dialogos')
-    te.classList.toggle('p-i-none')
-    document.getElementById('svgA'.destroy)
-
-}
-
-function toggleMenuB(){
-    let menu2 = document.querySelector('#quad-2');
-    let toggle2 = document.querySelector('#pega2');
-    menu2.classList.toggle('active');
-    toggle2.classList.toggle('active');
-    fundoPerso.classList.toggle('neutrum')
-    te.classList.toggle('p-i-none')
-}
-
-
-function toggleMenuC(){
-    let menu3 = document.querySelector('#quad-3');
-    let toggle3 = document.querySelector('#pega3');
-    menu3.classList.toggle('active');
-    toggle3.classList.toggle('active');
-    fundoPerso.classList.toggle('pomba')
-    te.classList.toggle('p-i-none')
 }
