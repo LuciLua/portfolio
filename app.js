c7250dee1373bcbcbb024c6f72d865a63658d210
LuciLua <luci.lua81@gmail.com>
2021-05-05 00:19:27 -0300

    :computer: Portfolio | Page Artes OK

--- app.js
+++ app.js
@@ -2,21 +2,3 @@ window.addEventListener('scroll', function(){
     const header = document.getElementById('memul')
     header.classList.toggle("sticky", window.scrollY > 0);
 });
-
-var modal = document.getElementById('janelaModal');
-
-var btnClose = document.getElementsByClassName("fechar")[0];
-
-
-function clique(img){
-    var modal = document.getElementById('janelaModal');
-    var modalImg = document.getElementById("imgModal");
-
-    modal.style.display="flex";
-    modalImg.src=img.src;
-    modalImg.alt=img.alt;
-}
-
-btnClose.onclick=function(){
-    modal.style.display="none";
-}
