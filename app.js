a58ff2bee8c111959c1f2f7c416606167c9da2fc
LuciLua <luci.lua81@gmail.com>
2021-05-09 15:55:48 -0300

    :computer: Slide Horizontal X

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll', function(){
     const header = document.getElementById('memul')
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function next(){
+    document.getElementById('quad').scrollLeft += 550;
+};
+
+function prev(){
+    document.getElementById('quad').scrollLeft -= 550;
+};
