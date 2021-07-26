899113917ab8cb04c985ce4de348f92cf07f275d
LuciLua <luci.lua81@gmail.com>
2021-07-26 11:16:50 -0300

    :computer: validation form contact

--- app.js
+++ app.js
@@ -14,8 +14,20 @@ function prev(){
 function send(){
     var btn = document.getElementById('btn-send')
     const inputs = document.querySelectorAll('.inpForm')
-    setTimeout(function(){
-        inputs.forEach(e=>{e.style.pointerEvents='none'; e.style.userSelect='none'; e.style.backgroundColor='#9a9a9a29'})
-        btn.style.pointerEvents='none'; btn.style.backgroundColor='#00ff62'; btn.style.color='#000'; btn.style.fontWeight='800'; btn.textContent='Enviado'; btn.style.userSelect='none'}, 500)
+    let reqs = 0
 
+    for (let i = 0; i < inputs.length; i++){
+        if(inputs[i].value.length != 0){
+            reqs++
+        }
+    }
+
+    if (reqs == inputs.length){
+        setTimeout(       
+            function(){
+            inputs.forEach(e=>{e.style.pointerEvents='none'; e.style.userSelect='none'; e.style.backgroundColor='#9a9a9a29'})
+            btn.style.pointerEvents='none'; btn.style.backgroundColor='#00ff62'; btn.style.color='#000'; btn.style.fontWeight='800'; btn.textContent='Enviado'; btn.style.userSelect='none'}, 500)
+    } else {
+    alert('ops... Você esqueceu de responder algo!')
+    }
 }
