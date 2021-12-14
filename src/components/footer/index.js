4bb91c8bed3fdc3a60b035b9113acfde6f9736ea
LuciLua <luci.lua81@gmail.com>
2021-12-14 16:55:29 -0300

    ✨ removed noopener

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,15 +7,15 @@ function Footer() {
             <br />
             <div className={styles.icosFooter}>
                 <div className={styles.ico}>
-                    <a target="_blank" rel="noopener" href="https://github.com/LuciLua"><img src="./github.svg" alt="GitHub"
+                    <a target="_blank" rel="noreferrer" href="https://github.com/LuciLua"><img src="./github.svg" alt="GitHub"
                         title="GitHub" /></a>
                 </div>
                 <div className={styles.ico}>
-                    <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/l%C3%BAcia-guelber-837a50185/"><img
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/l%C3%BAcia-guelber-837a50185/"><img
                         src="./linkedin.svg" alt="Linkedin" title="Linkedin" /></a>
                 </div>
                 <div className={styles.ico}>
-                    <a target="_blank" rel="noopener" href="https://br.pinterest.com/lucilua81"><img src="./pinterest.svg"
+                    <a target="_blank" rel="noreferrer" href="https://br.pinterest.com/lucilua81"><img src="./pinterest.svg"
                         alt="Pinterest" title="Pinterest" /></a>
                 </div>
             </div>
