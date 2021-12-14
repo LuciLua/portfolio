55dbbf08823d108d27b6b885987da074db182885
LuciLua <luci.lua81@gmail.com>
2021-12-14 16:54:00 -0300

    ✨ removed noopener

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -14,8 +14,8 @@ function Projects() {
                         <p>Aplicação desenvolvida com o framework Next.js</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://github.com/LuciLua/NLW5-Podcastr" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button>GitHub</button></a>
-                        <a target="_blank" rel="noopener" href="https://www.podcastr.ga/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/LuciLua/NLW5-Podcastr" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button>GitHub</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.podcastr.ga/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
                     </div>
                     <img src="./proj/podcastr.webp" alt="Projeto" />
                 </div>
@@ -25,7 +25,7 @@ function Projects() {
                         <p>Cenários e Edição da animação Neutrum</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://www.youtube.com/watch?v=VrI2ILQvzAw" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=VrI2ILQvzAw" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
                     </div>
                     <img src="./proj/neutrum.webp" alt="Projeto"/>
                 </div>
@@ -35,8 +35,8 @@ function Projects() {
                         <p>Jogo para navegador utilizando Javascript</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://github.com/LuciLua/snakegame" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
-                        <a target="_blank" rel="noopener" href="https://www.gamesnake.tk/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button>Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/LuciLua/snakegame" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.gamesnake.tk/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button>Acessar</button></a>
                     </div>
                     <img src="./proj/snake.webp" alt="Projeto"/>
                 </div>
@@ -46,8 +46,8 @@ function Projects() {
                         <p>Editor de texto Rich Text para navegador</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://github.com/LuciLua/RichTextEditor" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
-                        <a target="_blank" rel="noopener" href="https://rich-text-editor.vercel.app/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/LuciLua/RichTextEditor" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://rich-text-editor.vercel.app/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
                     </div>
                     <img src="./proj/rick.webp" alt="Projeto"/>
                 </div>
@@ -57,8 +57,8 @@ function Projects() {
                         <p>Aplicação em desenvolvimento utilizando o framework Next.js</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://github.com/LuciLua/calendar" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
-                        <a target="_blank" rel="noopener" href="http://calendar-blond.vercel.app/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/LuciLua/calendar" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
+                        <a target="_blank" rel="noreferrer" href="http://calendar-blond.vercel.app/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
                     </div>
                     <img src="./proj/calender.webp" alt="Projeto"/>
                 </div>
@@ -68,7 +68,7 @@ function Projects() {
                         <p>Produção audiovisual de moda e ensaio fotográfico</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://dialogosfuturismo1.wixsite.com/futurismo/video" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button>Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://dialogosfuturismo1.wixsite.com/futurismo/video" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button>Acessar</button></a>
                     </div>
                     <img src="./proj/dialogo.webp" alt="Projeto"/>
                 </div>
@@ -78,8 +78,8 @@ function Projects() {
                         <p>Protótipo de uma calculadora com JavaScript</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://github.com/LuciLua/calculadora" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
-                        <a target="_blank" rel="noopener" href="https://www.jscalculadora.tk/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/LuciLua/calculadora" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.jscalculadora.tk/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
                     </div>
                     <img src="./proj/calculadora.webp" alt="Projeto"/>
                 </div>
@@ -89,8 +89,8 @@ function Projects() {
                         <p>Jogo pedra papel tesousa em JavaScript</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://github.com/LuciLua/Rock-paper-and-scissors" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
-                        <a target="_blank" rel="noopener" href="https://www.gamejs-rps.tk/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/LuciLua/Rock-paper-and-scissors" className={styles.git}><img src="./gitgo.svg" title="Github" alt="Github"/><button >GitHub</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.gamejs-rps.tk/" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button >Acessar</button></a>
                     </div>
                     <img src="./proj/rock.webp" alt="Projeto"/>
                 </div>
@@ -100,7 +100,7 @@ function Projects() {
                         <p>Curta de animação em 3D feito no Blender</p>
                     </div>
                     <div className={styles.btnH}>
-                        <a target="_blank" rel="noopener" href="https://www.youtube.com/watch?v=d1iIKCi0Gvc" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button>Acessar</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=d1iIKCi0Gvc" className={styles.acc}><img src="./go.svg" alt="Acessar" title="Acessar"/><button>Acessar</button></a>
                     </div>
                     <img src="./proj/pomba.webp" alt="Projeto"/>
                 </div>
