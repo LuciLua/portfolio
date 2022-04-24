cd30efbe8d499f0f7e78645eb79d6b011ecace2a
LuciLua <luci.lua81@gmail.com>
2022-04-23 23:20:52 -0300

    🐑 Ok google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,16 @@ import Script from 'next/script'
 function App({ Component, pageProps }) {
     return (
         <>
-            <Script id="google-analytics" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-SKCXMJ02HX" />
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+            <Script id={`${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} strategy="lazyOnload">{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+            `}</Script>
             <Menu />
             <Component {...pageProps} />
             <Footer />
