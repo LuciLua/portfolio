cd9a50f4667f6c132e750910a2a00b26b7012d90
Luci Lua <luci.lua81@gmail.com>
2024-01-16 20:48:14 -0300

    create foote file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <footer className="w-full min-h-[80px] h-fit">
+            <div className="flex justify-center items-center w-full max-w-[1300px] h-fit">
+                footer
+            </div>
+        </footer>
+    )
+}
